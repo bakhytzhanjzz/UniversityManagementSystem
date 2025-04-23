9a8612e5a5e07923c0b6b0d5b41309a8b0ef9a6a
Bakhytzhan <bakhytzhanabdilmazhit@gmail.com>
2025-04-23 16:15:43 +0500

    Initial Commit

---
+++ src/test/java/com/bakhytzhan/ums/UniversityManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bakhytzhan.ums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
